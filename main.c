8c049164730ef13a9817a845e6bd9d6e5784a3bc
Vishnu <167505080+kratoes6@users.noreply.github.com>
2025-04-10 03:21:32 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
+int int main(int argc, char const *argv[])
+{
+	InputBuffer* input_buffer=new_input_buffer();
+	while(True){
+		print_prompt();
+		read_input(input_buffer);
+		if(strcmp(input_buffer->buffer,".exit")==0){
+			close_input_buffer(input_buffer);
+			exit(EXIT_SUCCESS);
 
+		}
+		else{
+			printf("Unrecognized Command  '%s'.\n",input_buffer->buffer);
+		}
+	}
+	/* code */
+	return 0;
+}
